aae701f605cabed74045ce80793637f2e9473e39
kanakdeepa <kanakdeepa.pradhan91@gmail.com>
2019-08-27 21:33:54 +0800

    Implemented textbox component

---
+++ src/components/common/TextBox/TextBox.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './TextBox.scss';
+
+class TextBox extends Component {
+  constructor() {
+    super();
+    this.state = {
+      textValue: ''
+    };
+  }
+
+  changeHandler = evt => {
+    this.setState({ textValue: evt.target.value });
+    this.props.onTextChange(evt);
+  };
+
+  render() {
+    const { placeholder, name } = this.props;
+    const { textValue } = this.state;
+
+    return (
+      <div className="textbox-container">
+        {textValue ? (
+          <span className="tiny-placeholder">{placeholder}</span>
+        ) : null}
+
+        <input
+          type="text"
+          name={name}
+          placeholder={placeholder}
+          onChange={this.changeHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default TextBox;
